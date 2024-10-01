17b08875ee776edc24e82f5417e3385558f1a026
loganamcnichols <loganamcnichols@gmail.com>
2024-10-01 16:24:40 -0600

    updates

--- main.go
+++ main.go
@@ -277,7 +277,7 @@ func createResponse() (uuid.UUID, error) {
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Security-Policy", "frame-src 'self' https://yourdomain.com;")
+	w.Header().Set("Content-Security-Policy", "frame-src *;")
 	var responseID uuid.UUID
 	cookie, err := r.Cookie("response-id")
 	if err == http.ErrNoCookie {
